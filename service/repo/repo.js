61fa7c8b69c989f623185fdfe7785759da2a4aa0
KeeBooMac <harishmagnate@harishmag.local>
2022-12-10 21:18:06 +0530

    use key phrases

--- service/repo/repo.js
+++ service/repo/repo.js
@@ -1,14 +1,17 @@
 const driver = require('bigchaindb-driver')
+const bip39 = require('bip39')
 
 // BigchainDB server instance or testnetwork (e.g. https://example.com/api/v1/)
 const API_PATH = 'http://localhost:9984/api/v1/'
 
 // Create a new keypair for Alice and Bob
-const alice = new driver.Ed25519Keypair()
-const bob = new driver.Ed25519Keypair()
+const charlie = new driver.Ed25519Keypair()
+const donald = new driver.Ed25519Keypair()
 
-console.log('Alice: ', alice.publicKey)
-console.log('Bob: ', bob.publicKey)
+console.log('charlie: ', charlie.publicKey)
+console.log('donald: ', donald.publicKey)
+
+const conn = new driver.Connection(API_PATH)
 
 // Define the asset to store, in this example
 // we store a bicycle with its serial number and manufacturer
@@ -24,24 +27,39 @@ const assetdata = {
 // E.g. the bicycle is fabricated on earth
 const metadata = { 'planet': 'earth' }
 
-// Construct a transaction payload
-const txCreateAliceSimple = driver.Transaction.makeCreateTransaction(
-    assetdata,
-    metadata,
-
-    // A transaction needs an output
-    [driver.Transaction.makeOutput(
-        driver.Transaction.makeEd25519Condition(alice.publicKey))
-    ],
-    alice.publicKey
-)
-
-// Sign the transaction with private keys of Alice to fulfill it
-const txCreateAliceSimpleSigned = driver.Transaction.signTransaction(txCreateAliceSimple, alice.privateKey)
-
 // Send the transaction off to BigchainDB
-const postData = () => {
-    const conn = new driver.Connection(API_PATH)
+const postData = (charlieSeed, donaldSeed) => {
+    const alice = new driver.Ed25519Keypair(charlieSeed);
+    const bob = new driver.Ed25519Keypair(donaldSeed)
+
+    console.log('Alice: ', alice)
+    console.log('Bob: ', bob.publicKey)  
+    
+    const assetdataa = {
+        'bicycle': {
+            'serial_number': 'adad',
+            'manufacturer': 'Bybye Inc.',
+        }
+    }
+    
+    // Metadata contains information about the transaction itself
+    // (can be `null` if not needed)
+    // E.g. the bicycle is fabricated on earth
+    const metadataa = { 'planet': 'balbla' }
+    console.info("adada")
+    // Construct a transaction payload
+    const txCreateAliceSimple = driver.Transaction.makeCreateTransaction(
+        assetdataa,
+        metadataa,
+        // A transaction needs an output
+        [driver.Transaction.makeOutput(
+            driver.Transaction.makeEd25519Condition(alice.publicKey))
+        ],
+        alice.publicKey
+    )
+    console.info("adad")
+    // Sign the transaction with private keys of Alice to fulfill it
+    const txCreateAliceSimpleSigned = driver.Transaction.signTransaction(txCreateAliceSimple, alice.privateKey)
 
     conn.postTransactionCommit(txCreateAliceSimpleSigned)
         .then(retrievedTx => console.log('Transaction', retrievedTx.id, 'successfully posted.'))
--- service/routes/index.js
+++ service/routes/index.js
@@ -1,10 +1,15 @@
-var express = require('express');
-var router = express.Router();
-var repo = require('../repo/repo.js');
+const express = require('express');
+const router = express.Router();
+const repo = require('../repo/repo.js');
+const bip39 = require('bip39')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  repo.postData();
+router.get('/', function (req, res, next) {
+  bip39.mnemonicToSeed("charlie").then(charlieSeed => {
+    bip39.mnemonicToSeed("donald").then(donaldSeed =>
+      repo.postData(charlieSeed.slice(0, 32), donaldSeed.slice(0, 32)));
+  }
+  );
   res.render('index', { title: 'Express' });
 });
 
